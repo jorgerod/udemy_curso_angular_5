eef8ccdce3258fe80207d4dec0a84cb7b531fa7d
Jorge Rodriguez <jorgerod84@gmail.com>
2018-03-03 10:53:17 +0100

    First commit

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'empleado',
+	templateUrl: './empleado.component.html'
+})
+
+
+export class EmpleadoComponent {
+	public title = 'Empleado';
+}
