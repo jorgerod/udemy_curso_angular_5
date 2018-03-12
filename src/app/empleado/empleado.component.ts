5942090d6ba0d39094cd37d94c54da28a37357f8
jrodrig1 <Valencia18>
2018-03-12 08:20:32 +0100

    Modelos

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Empleado} from './empleado';
 
 @Component({
 	selector: 'empleado',
@@ -8,4 +9,15 @@ import {Component} from '@angular/core';
 
 export class EmpleadoComponent {
 	public title = 'Componente Empleado';
+	public empleado:Empleado;
+	public trabajadores:Array<Empleado>;
+
+	constructor() {
+		this.empleado = new Empleado('Pepe', 45, 'Cocinero', true);
+		this.trabajadores = [
+			new Empleado('Pepe', 45, 'Cocinero', true),
+			new Empleado('Manolo', 50, 'Administrativo', true),
+			new Empleado('Ana', 35, 'Cocinera', true)
+		];
+	}
 }
